c2005c5eba9578db97cfa62b86e71b576873170e
Dravin Flores <dravinflores@gmail.com>
2020-10-25 19:43:40 -0400

    Initial commit.

---
+++ program/source/main.py
@@ -0,0 +1,26 @@
+###############################################################################
+#
+#   File: main.py
+#
+#   Author: Dravin Flores <flores69@msu.edu>
+#
+#   Created: 24 October 2020
+#
+#   Last Modified: 24 October 2020
+#
+#   Purpose: This program acts as the main GUI-based interface to the database
+#       and Swager Station for the sMDT Project. 
+#
+###############################################################################
+
+# These are all the modules that are required for this program to run.
+import platform
+import sys
+import os
+
+
+# These are the global variables used in this program.
+system = platform.system()
+dbRelativePath = "sMDT/Database"
+dbAbsolutePath = ""
+
